8322c22e3dab2e97cc03b5da3c068f2fe4df8277
Yogesh <yogeshkunkawlekar12@gmail.com>
2023-07-29 17:25:19 +0530

    Create datevalidation, dropdown validation, subtask validation

---
+++ calendar.js
@@ -0,0 +1,38 @@
+var startDateInput = document.getElementById("start_date");
+var endDateInput = document.getElementById("end_date");
+
+function enableEndDate() {
+  if (startDateInput.value !== "") {
+    endDateInput.min = startDateInput.value;
+    endDateInput.disabled = false;
+  } else {
+    endDateInput.value = "";
+    endDateInput.disabled = true;
+  }
+}
+
+function enableStatusDropdown() {
+  const statusDropdown = document.getElementById("status");
+  var currentDate = moment().format("YYYY-MM-DD");
+  console.log(currentDate);
+
+  statusDropdown.disabled = false;
+  console.log(endDateInput.value);
+
+  if (endDateInput.value > currentDate) {
+    alert("more");
+    statusDropdown.options[3].disabled = true;
+    statusDropdown.options[1].disabled = false;
+
+    return;
+  } else if (endDateInput.value < currentDate) {
+    statusDropdown.options[1].disabled = true;
+    statusDropdown.options[3].disabled = false;
+
+    return;
+    // Code to be executed if condition1 is false and condition2 is true
+  } else {
+    // statusDropdown.disabled = true;
+    // Code to be executed if both condition1 and condition2 are false
+  }
+}
---
+++ navBar.js
@@ -0,0 +1,35 @@
+var onPage =  "add_task";
+
+//change side
+function handleAddClick() {
+    if (onPage != "add_task") {
+      const addSide = document.getElementById("add_task");
+  
+      addSide.style.display = "";
+  
+      const viewTask = document.getElementById("view_task");
+  
+      viewTask.style.display = "none";
+      onPage = "add_task";
+    }
+  }
+  
+  async function handleViewClick() {
+    if (onPage !== "view_task") {
+      const addSide = document.getElementById("add_task");
+  
+      addSide.style.display = "none";
+      const viewTask = document.getElementById("view_task");
+  
+      viewTask.style.display = "";
+      onPage = "view_task";
+  
+    //   await getEmpDetails();
+    }
+  }
+  
+  const addBarElement = document.querySelector(".add_bar");
+  const viewBarElement = document.querySelector(".view_bar");
+  addBarElement.addEventListener("click", handleAddClick);
+  viewBarElement.addEventListener("click", handleViewClick);
+  
